ff3bd61294c1d47f9e8f7a3dc6103db98973d35c
U545959 <stephen_jasper@intra.bca>
2022-07-06 17:36:09 +0700

    Routing + login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BatchLoginComponent} from "./batch-login/batch-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: BatchLoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BatchLoginComponent } from './batch-login/batch-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatchLoginComponent
   ],
   imports: [
     BrowserModule,
